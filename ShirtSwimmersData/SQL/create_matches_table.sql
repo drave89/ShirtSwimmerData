7bdbab26c3985a19c4101df9f3a150a62d2c2218
Scott Vig <svig@brandt.ca>
2020-09-21 12:43:24 -0600

    Add SQL scripts for data generation

---
+++ ShirtSwimmersData/SQL/create_matches_table.sql
@@ -0,0 +1,32 @@
+﻿USE [Shirtswimmers]
+GO
+
+/****** Object:  Table [dbo].[Matches]    Script Date: 2020-09-21 12:41:53 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Matches](
+	[match_id] [bigint] NOT NULL,
+	[dire_score] [int] NULL,
+	[radiant_score] [int] NULL,
+	[duration] [int] NULL,
+	[game_mode] [int] NULL,
+	[lobby_type] [int] NULL,
+	[skill] [int] NULL,
+	[start_time] [int] NULL,
+	[tower_status_dire] [int] NULL,
+	[tower_status_radiant] [int] NULL,
+	[radiant_win] [bit] NULL,
+	[patch] [int] NULL,
+	[region] [int] NULL,
+ CONSTRAINT [PK_Matches] PRIMARY KEY CLUSTERED 
+(
+	[match_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
---
+++ ShirtSwimmersData/SQL/create_players_table.sql
@@ -0,0 +1,50 @@
+﻿USE [Shirtswimmers]
+GO
+
+/****** Object:  Table [dbo].[Players]    Script Date: 2020-09-21 12:42:45 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Players](
+	[match_id] [bigint] NOT NULL,
+	[player_slot] [int] NULL,
+	[account_id] [bigint] NULL,
+	[kills] [int] NULL,
+	[deaths] [int] NULL,
+	[assists] [int] NULL,
+	[last_hits] [int] NULL,
+	[denies] [int] NULL,
+	[gold] [int] NULL,
+	[gold_per_min] [int] NULL,
+	[gold_spent] [int] NULL,
+	[hero_damage] [int] NULL,
+	[hero_healing] [int] NULL,
+	[tower_damage] [int] NULL,
+	[xp_per_min] [int] NULL,
+	[personaname] [varchar](max) NULL,
+	[isRadiant] [bit] NULL,
+	[win] [int] NULL,
+	[lose] [int] NULL,
+	[total_gold] [int] NULL,
+	[total_xp] [int] NULL,
+	[kda] [int] NULL,
+	[abandons] [int] NULL,
+	[hero_id] [int] NULL,
+	[item_0] [int] NULL,
+	[item_1] [int] NULL,
+	[item_2] [int] NULL,
+	[item_3] [int] NULL,
+	[item_4] [int] NULL,
+	[item_5] [int] NULL,
+	[item_neutral] [int] NULL,
+	[backpack_0] [int] NULL,
+	[backpack_1] [int] NULL,
+	[backpack_2] [int] NULL,
+	[party_size] [int] NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
